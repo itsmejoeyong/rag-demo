92013d0b9f405929c625e1f2a6fd3c8ffb95d7a7
JoeYong01 <joeanselmyz@gmail.com>
2024-05-27 08:44:24 +0800

    fixed inconsistent instance variable naming from model > model_name

--- src/custom_embeddings_function/voyageai_embeddings_function.py
+++ src/custom_embeddings_function/voyageai_embeddings_function.py
@@ -10,13 +10,13 @@ from chromadb import Documents, EmbeddingFunction, Embeddings
 class VoyageAIEmbeddingFunction(EmbeddingFunction[Documents]):
     def __init__(self, api_key: str, model_name: str, input_type: str):
         self.api_key = api_key
-        self.model = model_name
+        self.model_name = model_name
         self.input_type = input_type
         self._client = voyageai.Client(api_key=api_key)
 
     def __call__(self, input: Documents) -> Embeddings:
         input = [text.replace("\n", " ") for text in input]
         result = self._client.embed(
-            texts=input, model=self.model, input_type=self.input_type
+            texts=input, model=self.model_name, input_type=self.input_type
         )
         return cast(Embeddings, result.embeddings)
